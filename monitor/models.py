963b29d43dca2003c2ab36f2faca854ef2f5a333
Shubh Pathak <shubhpathak07@gmail.com>
2021-04-22 01:28:41 +0530

    Added Plant and Device Model

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Plant(models.Model):
+    """
+    Stores the Data Received from Sensor
+    """
+    name = models.CharField(max_length=128)
+    moisture = models.PositiveIntegerField()
+    temperature = models.PositiveIntegerField()
+    humidity = models.PositiveIntegerField()
+    light = models.PositiveIntegerField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return (self.name)[:15]
+
+
+class Device(models.Model):
+    """
+    Stores the Information About Device
+    """
+    user = models.ForeignKey(User,models.CASCADE)
+    unique_id = models.SlugField(max_length = 10,unique=True)
+    plant = models.ForeignKey(Plant,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Device ID: {unique_id}"
